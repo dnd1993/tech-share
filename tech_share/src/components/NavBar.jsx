66749f06a2b6dd066f840fda46fcdb66109b1755
dnd1993 <dnd1993@yandex.ru>
2022-11-16 11:41:05 -0500

    [feat] Style the NavBar

--- tech_share/src/components/NavBar.jsx
+++ tech_share/src/components/NavBar.jsx
@@ -5,26 +5,28 @@ import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
   const activeStyle = {
-    textDecoration: 'underline',
+    backgroundColor: 'green',
   };
   return (
-    <nav className="flex justify-center">
+    <nav className="flex justify-center mb-5">
       <NavLink
         to="/"
         style={({ isActive }) => (isActive ? activeStyle : undefined)}
+        className="px-5 py-4 bg-black rounded-md text-white text-2xl hover:text-white hover:bg-red-700"
       >
         Home
       </NavLink>
       <NavLink
         to="/example-one"
         style={({ isActive }) => (isActive ? activeStyle : undefined)}
-        className="mx-5"
+        className="px-5 py-4 bg-black rounded-md text-white text-2xl mx-7 hover:text-white hover:bg-red-700"
       >
         useState#1
       </NavLink>
       <NavLink
         to="/example-two"
         style={({ isActive }) => (isActive ? activeStyle : undefined)}
+        className="px-5 py-4 bg-black rounded-md text-white text-2xl hover:text-white hover:bg-red-700"
       >
         useState#2
       </NavLink>
